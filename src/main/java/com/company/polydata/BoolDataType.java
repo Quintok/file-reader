f175ced82121cf12bed82f372d10e3365df288b1
Nick Cronin <nick@cronin.id.au>
2015-05-12 20:09:32 +0100

    Massive hack to get bool to work
    I can’t understand why bool behaves this way but it seems like it
    optionally has two bytes when serialised??  weird.

--- src/main/java/com/company/polydata/BoolDataType.java
+++ src/main/java/com/company/polydata/BoolDataType.java
@@ -1,12 +1,34 @@
 package com.company.polydata;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.nio.ByteBuffer;
 
 public class BoolDataType implements DataType<BoolDataType> {
+    Logger logger = LoggerFactory.getLogger(BoolDataType.class);
     boolean value;
     @Override
     public BoolDataType read(ByteBuffer buffer) {
-        this.value = buffer.get() > 0;
+        byte b = buffer.get();
+        byte oldB = b;
+        boolean reRead = false;
+
+        // TODO: Work out what the hell is going on here.
+        if(b > 1 && b == 22) {
+            reRead = true;
+            b = buffer.get();
+        }
+        if(reRead)
+            logger.error("Re reading boolean polyVal for no obvious reason.  b={}, oldB={}, position={}", b, oldB, buffer.position());
+        this.value = b > 0;
         return this;
+    }
+
+    @Override
+    public String toString() {
+        return "BoolDataType{" +
+                "value=" + value +
+                '}';
     }
 }
