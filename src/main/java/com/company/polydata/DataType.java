c929e8866c80ed0f59b9fcd0024bf1fb9d65dd00
Nick Cronin <nick@cronin.id.au>
2015-05-12 12:49:54 +0100

    damn you spelling.

--- src/main/java/com/company/polydata/DataType.java
+++ src/main/java/com/company/polydata/DataType.java
@@ -3,7 +3,7 @@ package com.company.polydata;
 import java.nio.ByteBuffer;
 
 /**
- * Ideally this would be an abstrat class as the constructor would be used instead of the read method
+ * Ideally this would be an abstract class as the constructor would be used instead of the read method
  * Unfortunately BinaryDataType would require multiple inheritance in that case.
  */
 public interface DataType<T extends DataType<T>> {
